15b569703607ad45356e7b23025e6a99edb5902c
Cristiano Silva Souza <msp.cris@gmail.com>
2025-10-02 15:44:50 -0300

    Criado json com cad_endereco .env com dados da api do chatgpt

--- project.py
+++ project.py
@@ -4,3 +4,5 @@
 
 # Tais como: Médico escreve queixa: dor de cabeça ou cefaleia. Em uma barra lateral do sistema vai aparecer de forma clara que o cliente se queixou de dor de cabeça, mesmo que esteja escrito cefaleia agora. É importante saber termos médicos. E mostrar ao médico atendente que o caso é reincidente e quais prontuários estão as reclamações anteriores do paciente, etc. 
 
+# Primeiro eu vou fazer o programa enviar os dados para analise da IA, criando um atendimento e vendo as sugestões, depois, vou fazer isso de forma interativa e inline. Médico digitou cefaleia, nem precisa sair do campo, já vou capturar o dado (Janderson) e vou fazer a pesquisa com a IA e receber o resultado neste espaço lateral planejado acima.
+
