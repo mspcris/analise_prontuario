e5bba150105f8ce3bc03cb06145f53803866f4e3
Cristiano Silva Souza <msp.cris@gmail.com>
2025-10-02 15:23:59 -0300

    Git initial

---
+++ project.py
@@ -0,0 +1,6 @@
+# Este é o primeiro objetivo de muitos que existirão:
+
+# Objetivo é saber quem o médico está atendendo e enviar para IA os últimos atendimentos daquele paciente e com estes dados anteriores, fazer sugestões e insights. 
+
+# Tais como: Médico escreve queixa: dor de cabeça ou cefaleia. Em uma barra lateral do sistema vai aparecer de forma clara que o cliente se queixou de dor de cabeça, mesmo que esteja escrito cefaleia agora. É importante saber termos médicos. E mostrar ao médico atendente que o caso é reincidente e quais prontuários estão as reclamações anteriores do paciente, etc. 
+
